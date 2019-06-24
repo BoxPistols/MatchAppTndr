a2e4674241997cf5e9ddf9939c58bdfa2a5bcea2
ait <ait@ex.com>
2019-06-24 22:57:49 +0900

    add user db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # get 'top/index'
-  root "top#index"  
+  root "top#index"
+
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 end
