4a8a364ba82b1e7c82f25d626f6f1ac1804ec50d
ait <ait@ex.com>
2019-06-27 11:01:23 +0900

    Add img

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Add
+  mount_uploader :img_name, ImgNameUploader
 end
