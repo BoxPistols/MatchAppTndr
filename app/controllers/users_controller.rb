298258c40bdf4279c4256d205aea4f232ff9494c
ait <ait@ex.com>
2019-06-30 18:47:03 +0900

    add show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-def show
-  # @userというインスタンス変数を用意する
-  @user = User.find_by(id: params[:id])  
-end
+  def show
+    # @userというインスタンス変数を用意する
+    @user = User.find_by(id: params[:id])
+  end
 
 end
